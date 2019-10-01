22a4d4dd148bbe2f2c78f1a8c00dfa08631a3104
Daniel Leech <dan.t.leech@gmail.com>
2019-10-01 16:29:57 +0100

    Move console bootstrap to container

--- tests/TestContainer.php
+++ tests/TestContainer.php
@@ -5,6 +5,7 @@ namespace Inviqa\Spryker\Debug\Tests;
 use Inviqa\Spryker\Debug\Shared\Test\ApplicationBuilder;
 use Silex\Application;
 use Spryker\Service\Container\Container;
+use Spryker\Zed\Console\Communication\ConsoleBootstrap;
 use Spryker\Zed\Product\Business\ProductFacade;
 use Spryker\Zed\Product\Business\ProductFacadeInterface;
 use Symfony\Component\Debug\Debug;
@@ -17,7 +18,7 @@ class TestContainer extends Container
 
         parent::__construct([
             Application::class => ApplicationBuilder::create(__DIR__ . '/App', 'GB')->build(),
-
+            ConsoleBootstrap::class => new ConsoleBootstrap(),
             ProductFacadeInterface::class => function () {
                 return new ProductFacade();
             }
