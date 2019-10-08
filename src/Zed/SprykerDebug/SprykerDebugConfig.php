307d0791cd237dc5094ae615be650cb1447f81c2
Daniel Leech <dan.t.leech@gmail.com>
2019-10-08 09:56:54 +0100

    Add explicit config for API base URL

--- src/Zed/SprykerDebug/SprykerDebugConfig.php
+++ src/Zed/SprykerDebug/SprykerDebugConfig.php
@@ -10,6 +10,10 @@ class SprykerDebugConfig extends AbstractBundleConfig
 {
     public function getYvesBaseUrl()
     {
+        if (Config::hasKey(SprykerDebugConstants::API_BASE_URL)) {
+            return Config::get(SprykerDebugConstants::API_BASE_URL);
+        }
+
         return Config::get(Config::get(ApplicationConstants::YVES_SSL_ENABLED) ?
             ApplicationConstants::BASE_URL_SSL_YVES :
             ApplicationConstants::BASE_URL_YVES
