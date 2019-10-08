cee0bf4ce6ecfa9349c087bf0dec3e672a2805a0
Daniel Leech <dan.t.leech@gmail.com>
2019-10-08 14:35:46 +0100

    Adds failing scenario for entity debug

--- tests/Acceptance/Context/DatabaseContext.php
+++ tests/Acceptance/Context/DatabaseContext.php
@@ -2,13 +2,25 @@
 
 namespace Inviqa\SprykerDebug\Tests\Acceptance\Context;
 
+use Behat\Gherkin\Node\TableNode;
+use Behat\Behat\Tester\Exception\PendingException;
 use Behat\Behat\Context\Context;
 use Behat\Behat\Hook\Scope\ScenarioScope;
+use Orm\Zed\Product\Persistence\SpyProductAbstract;
+use Propel\Runtime\ActiveRecord\ActiveRecordInterface;
+use Propel\Runtime\Map\TableMap;
 use Propel\Runtime\Propel;
 use RuntimeException;
+use function Safe\json_decode;
+use Safe\Exceptions\JsonException;
 
 class DatabaseContext implements Context
 {
+    /**
+     * @var array
+     */
+    private $map;
+
     /**
      * @BeforeScenario @db-transaction
      */
@@ -37,5 +49,34 @@ class DatabaseContext implements Context
                 )
             );
         }
+    }
+
+    /**
+     * @Given the entity :arg1 exists as :arg2:
+     */
+    public function theTableHasTheFollowingData(string $className, $tag, TableNode $table)
+    {
+        $entity = new $className;
+
+        $data = array_map(function($value) {
+            try {
+                return json_decode($value, true);
+            } catch (JsonException $e) {
+                if (preg_match('{<(.*)>}', $value, $matches)) {
+                    $key = $matches[1];
+                    if (!isset($this->map[$key])) {
+                        throw new RuntimeException(sprintf(
+                            'No previously persisted entity with tag "%s" exists, I have "%s"',
+                            $key, implode('", "', array_keys($this->map))
+                        ));
+                    }
+                    return $this->map[$key];
+                }
+            }
+        }, $table->getRowsHash());
+
+        $entity->fromArray($data, TableMap::TYPE_PHPNAME);
+        $entity->save();
+        $this->map[$tag] = $entity->getPrimaryKey();
     }
 }
